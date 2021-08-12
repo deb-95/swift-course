d862a06ea08ca8aa4a8d34b5b776b38d97809be6
Debora Del Vecchio <deboradelvecchio9@gmail.com>
2021-08-12 12:56:46 +0200

    finished xylophone

--- 7.xylophone/Xylophone/ViewController.swift
+++ 7.xylophone/Xylophone/ViewController.swift
@@ -7,15 +7,36 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    
+    fileprivate func dimButton(_ sender: UIButton) {
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2 ) {
+            UIView.animate(withDuration: 0.2) {
+                sender.alpha = 1.0
+            }
+        }
+    }
+    
+    @IBAction func onKeyPressed(_ sender: UIButton) {
+        let title = sender.title(for: .normal)!
+        playSound(title)
+        dimButton(sender)
+    }
+    
+    func playSound(_ note: String) {
+        let url = Bundle.main.url(forResource: note, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player!.play()
+    }
 }
 
