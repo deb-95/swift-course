40ec3066f7d256d3ef1698e52262b5befdb31ccc
Debora Del Vecchio <deboradelvecchio9@gmail.com>
2021-09-09 10:14:29 +0200

    added 16.i-am-rich-swiftUI

---
+++ 16.i-am-rich-swiftui/I Am Rich SwiftUI/Shared/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Debora Del Vecchio on 09/09/21.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
